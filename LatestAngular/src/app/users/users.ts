26528a2602571b497d49ffcd5f693ca6594bec79
Saptarshi Biswas <mathsaptarshi@gmail.com>
2026-01-31 17:25:15 +0530

    Update

--- LatestAngular/src/app/users/users.ts
+++ LatestAngular/src/app/users/users.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TestService } from '../services/test-service';
 
 @Component({
   selector: 'app-users',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './users.css',
 })
 export class Users {
+  private _testService = inject(TestService);
+  userList = signal<any[]>([])
 
+  constructor(){
+    this._testService.getUserList().subscribe((data:any)=>{
+      console.log(data)
+      this.userList.set(data.data)
+    })
+  }
 }
