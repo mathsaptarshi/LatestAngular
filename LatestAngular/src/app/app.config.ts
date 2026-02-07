1a6c3ec3d50a3bd2530e7d3c516f478488aefba8
Saptarshi Biswas <mathsaptarshi@gmail.com>
2026-02-07 13:07:37 +0530

    Updated

--- LatestAngular/src/app/app.config.ts
+++ LatestAngular/src/app/app.config.ts
@@ -8,12 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-<<<<<<< HEAD:LatestAngularApp/src/app/app.config.ts
     provideHttpClient(),
     provideRouter(routes)
-=======
-    provideRouter(routes),
-    provideHttpClient()
->>>>>>> 5bd34f6fd5296530f7ab97ae57f28e968a3cd719:LatestAngular/src/app/app.config.ts
   ]
 };
